a66c8a58550e70b15b3ebe3d90a2672d3f2a52cb
John Rix <41085020+john-rix@users.noreply.github.com>
2020-10-31 16:40:49 -0400

    updating etch-a-sketch

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,21 +1,21 @@
 //define initial grid size
-var size = 40;
+var size = 50;
 
 //build the grid layout
-const container = document.querySelector('#container');
+const grid = document.querySelector('#grid');
 function buildGrid(size) {
-    container.style.cssText = `grid-template-columns: repeat(${size}, auto)`;
+    grid.style.cssText = `grid-template-columns: repeat(${size}, auto)`;
     for (i=0; i<(size*size); i++) {
         let pixel = document.createElement('div');
         pixel.classList.add("pixel")
-        container.appendChild(pixel);
+        grid.appendChild(pixel);
     };
 };
 buildGrid(size);
 
 //change color of pixels on mouseover
 function draw() {
-const pixels = document.querySelectorAll('.pixel');
+    const pixels = document.querySelectorAll('.pixel');
         pixels.forEach((pixel) => {
             pixel.addEventListener('mouseover', (e) => {
                 e.target.style.background = document.querySelector('#colorPicker').value;
@@ -24,12 +24,21 @@ const pixels = document.querySelectorAll('.pixel');
 };
 draw();
 
+//clear grid of color
+function clear() {
+    const pixels = document.querySelectorAll('.pixel');
+    pixels.forEach((pixel) => {
+        pixel.style.background = 'white';
+    })
+}
+const clearButton = document.querySelector('#clear')
+clearButton.addEventListener('click', clear)
+
 //reset the grid and rebuild with new dimensions
-//need to set a min and max for input, only allow integers****
 const resetButton = document.querySelector('#reset');
 resetButton.addEventListener('click', reset);
 function reset() {
-    container.textContent = '';
+    grid.textContent = '';
     size = window.prompt('Enter number of cells per side');
     buildGrid(size);
     draw()
